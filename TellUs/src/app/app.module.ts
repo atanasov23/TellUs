f13acf845b17836d3fb7d0dd5ea4c8fd8c2a5453
Kristian Atanasov <124519664+atanasov23@users.noreply.github.com>
2023-07-14 21:58:49 +0300

    Create header and navigation.

--- TellUs/src/app/app.module.ts
+++ TellUs/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './core/header/header.component';
+import { NavigationComponent } from './core/navigation/navigation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavigationComponent,
   ],
   imports: [
     BrowserModule,
