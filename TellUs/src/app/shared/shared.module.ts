4bb640fdc79eb35d98130832ec41932058c75843
Kristian Atanasov <124519664+atanasov23@users.noreply.github.com>
2023-07-23 14:39:35 +0300

    Adding navigation link to logo component.

--- TellUs/src/app/shared/shared.module.ts
+++ TellUs/src/app/shared/shared.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AppLogoComponent } from './components/app-logo/app-logo.component';
 import { UserProfileImageComponent } from './components/user-profile-image/user-profile-image.component';
+import { AppRoutingModule } from '../app-routing.module';
 
 @NgModule({
   declarations: [
@@ -9,7 +10,8 @@ import { UserProfileImageComponent } from './components/user-profile-image/user-
     UserProfileImageComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    AppRoutingModule
   ],
   exports: [AppLogoComponent,UserProfileImageComponent]
 })
